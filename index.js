202809a9bd4b1259c9acb40d0f6d53b45bb2c8f0
MacBook Pro <naym100m@gmail.com>
2024-03-29 21:44:27 +0600

    create  a task deleted route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,10 +42,20 @@ async function run() {
       .collection("taskMateTasks");
 
     // Task collection
+    app.delete("taskMate/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete id----", id);
+      const query = { _id: new ObjectId(id) };
+      console.log("delete", query);
+      const result = await taskMateTasksCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/taskMate/tasks", async (req, res) => {
       const result = await taskMateTasksCollection.find().toArray();
       res.send(result);
     });
+    
     app.post("/taskMate/tasks", async (req, res) => {
       const task = req.body;
       const result = await taskMateTasksCollection.insertOne(task);
