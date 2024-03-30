f7f063187afe9978f03c71440ea291551dc43d9e
MacBook Pro <naym100m@gmail.com>
2024-03-30 22:33:13 +0600

    update a live site link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // Middle ware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://task-mate-app.netlify.app"],
     credentials: true,
   })
 );
